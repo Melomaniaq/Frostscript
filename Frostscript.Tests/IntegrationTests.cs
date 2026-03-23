9c7ef8f1a121d9169dad68fcbc2f825a5ced370d
Aisling Proulx <aislingproulx@outlook.com>
2026-03-23 18:30:33 -0400

    add precedence tests

--- Frostscript.Tests/IntegrationTests.cs
+++ Frostscript.Tests/IntegrationTests.cs
@@ -19,6 +19,10 @@ namespace Frostscript.Tests
         [InlineData("2 + 4 * 2", 10)]
         [InlineData("2 + 2 == 1 * 4", true)]
         [InlineData("2 + 2 != 1 * 4", false)]
+        [InlineData("2 + 2 >= 1 * 4", true)]
+        [InlineData("2 + 2 > 1 * 4", false)]
+        [InlineData("2 + 2 <= 1 * 4", true)]
+        [InlineData("2 + 2 < 1 * 4", false)]
         public void Precedence(string script, dynamic expected)
         {
             Assert.Equal(expected, Frostscript.Run<dynamic>(script));
