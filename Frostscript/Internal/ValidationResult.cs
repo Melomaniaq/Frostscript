53da190bb0e87de406caeeae22d39bdd08776ef9
Aisling Proulx <aislingproulx@outlook.com>
2026-03-27 14:30:38 -0400

    update validation result

--- Frostscript/Internal/ValidationResult.cs
+++ Frostscript/Internal/ValidationResult.cs
@@ -1,11 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Frostscript.Expressions;
 
 namespace Frostscript.Internal
 {
     internal interface IValidationResult;
 
-    internal record Pass(INode Node) : IValidationResult;
-    internal record Fail(string Error, Token Token) : IValidationResult;
+    internal sealed record Pass(IExpression Expression) : IValidationResult;
+    internal sealed record Fail(Token Token, params string[] Errors) : IValidationResult;
+
+    internal static class IValidationResultExtentions
+    {
+        public static IValidationResult MergeErrors(this IValidationResult result1, IValidationResult result2) => result1 switch
+        {
+            Pass => result2,
+            Fail fail => result2 switch
+            {
+                Pass pass => fail,
+                Fail previouseFail => new Fail(
+                    fail.Token,
+                    [.. previouseFail.Errors, .. fail.Errors]
+                ),
+                _ => throw new InvalidOperationException("Unexpected validation result")
+            },
+            _ => throw new InvalidOperationException("Unexpected validation result")
+        };
+
+        public static IValidationResult Map(this IValidationResult result, Func<IExpression, IExpression> mapFunc) => result switch
+        {
+            Pass pass => new Pass(mapFunc(pass.Expression)),
+            Fail fail => fail,
+            _ => throw new InvalidOperationException("Unexpected validation result")
+        };
+
+        public static IValidationResult Bind(this IValidationResult result, Func<IExpression, IValidationResult> bindFunc) => result switch
+        {
+            Pass pass => bindFunc(pass.Expression),
+            Fail fail => fail,
+            _ => throw new InvalidOperationException("Unexpected validation result")
+        };
+    }
 }
